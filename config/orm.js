7397b76573a6ad09421cfcb7e39801e2bf938b6a
z1995 <zhannaavanesova@yahoo.com>
2019-01-31 19:59:26 -0700

    Changes

--- config/orm.js
+++ config/orm.js
@@ -6,7 +6,20 @@ var orm = {
         connection.query(queryString, [table], function(err, result) {
             if (err) throw err;
             console.log(result);
+        });
+    },
+    insertOne: function(table, burger_name, name) {
+        var queryString = "INSERT INTO ?? (??) VALUES (?)";
+        connection.query(queryString, [table, burger_name, name], function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    updateOne: function(table, burger_eaten, id) {
+        var queryString = "UPDATE ?? SET ? WHERE id= ?";
+        connection.query(queryString, [table, burger_eaten, id], function(err, result) {
+            if (err) throw err;
+            console.log(result);
         })
     }
-
 }
